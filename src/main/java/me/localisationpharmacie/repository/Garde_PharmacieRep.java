0ee0ea2d46118c0421ac5efe7c3454b553212446
AnasJeg <anasjegoual.2000@gmail.com>
2023-03-21 13:52:34 +0000

    service dist

--- src/main/java/me/localisationpharmacie/repository/Garde_PharmacieRep.java
+++ src/main/java/me/localisationpharmacie/repository/Garde_PharmacieRep.java
@@ -4,7 +4,9 @@ package me.localisationpharmacie.repository;
 import me.localisationpharmacie.entity.Garde_Pharmacie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Garde_PharmacieRep extends JpaRepository<Garde_Pharmacie, Integer> {
@@ -15,4 +17,12 @@ public interface Garde_PharmacieRep extends JpaRepository<Garde_Pharmacie, Integ
 
     @Query("select gp from Garde_Pharmacie gp where CURRENT_DATE BETWEEN gp.pg.dateDebut and gp.dateFin ")
     List<Garde_Pharmacie> findAllPharmaciesEnGarde();
+
+
+    @Query(value="select gp.pharmacie, min(sqrt(power((gp.pharmacie.latitude-:lat),2)+power((gp.pharmacie.longitude-:long),2))) from Garde_Pharmacie gp where gp.pharmacie.zone.nom = :zone and gp.pharmacie.zone.ville.nom = :ville and gp.garde.type like 'jour'")
+    List<ArrayList> findDistanceJour(@Param("lat") double lat, @Param("long") double lon, @Param("zone") String zone, @Param("ville") String ville);
+
+    @Query(value="select gp.pharmacie, min(sqrt(power((gp.pharmacie.latitude-:lat),2)+power((gp.pharmacie.longitude-:long),2))) from Garde_Pharmacie gp where gp.pharmacie.zone.nom = :zone and gp.pharmacie.zone.ville.nom = :ville and gp.garde.type like 'nuit'")
+    List<ArrayList> findDistanceNuit(@Param("lat") double lat, @Param("long") double lon, @Param("zone") String zone, @Param("ville") String ville);
+
 }
